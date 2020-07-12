b49f736ce62b1a44fa9032c9196a67b973b2a063
Waddah Mortada <w.mortada87@gmail.com>
2020-07-12 11:37:13 +0100

    Add Board component

---
+++ src/components/Board.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Board = props => {
+  return (
+    <div>Board</div>
+  )
+}
+
+Board.PropTypes = {
+  type: PropTypes.any
+}
+
+export default Board
--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import TemplateSelector from './TempleteSelector'
+import Board from './Board'
 import { toTitleCase } from '../lib/helpers'
 
 const App = props => {
@@ -20,6 +21,7 @@ const App = props => {
       <h1>Retro Tool</h1>
       <h3>{toTitleCase(template)}</h3>
       <button onClick={() => setTemplate()}>New Board</button>
+      <Board type={template} />
     </div>
   )
 }
