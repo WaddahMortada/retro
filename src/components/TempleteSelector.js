ba2e0202a2f3b8c3e1254b6fd7ecabf6c970a1d2
Waddah Mortada <w.mortada87@gmail.com>
2020-07-13 16:47:21 +0100

    Add required flag template form

--- src/components/TempleteSelector.js
+++ src/components/TempleteSelector.js
@@ -17,14 +17,14 @@ const TemplateSelector = props => {
       <h2>Create Board</h2>
       <form onSubmit={create}>
         <h3>Template</h3>
-        <select onChange={(e) => setTemplate(e.target.value)} value={template}>
+        <select onChange={(e) => setTemplate(e.target.value)} value={template} required>
           <option value=""></option>
           <option value="mad_sad_glad">Mad Sad Glad</option>
           <option value="start_stop_continue">Start Stop Continue</option>
           <option value="liked_learnt_lacked">Liked Learnt Lacked</option>
         </select>
         <h3>Max votes per user (whole board)</h3>
-        <input type="number" min="1" step="1" pattern="\d+" value={voteLimit} onChange={(e) => setVoteLimit(e.target.value)} />
+        <input type="number" min="1" step="1" pattern="\d+" value={voteLimit} onChange={(e) => setVoteLimit(e.target.value)} required />
         <input type="submit" value="Create"/>
       </form>
     </div>
