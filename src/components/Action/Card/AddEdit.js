3db7382f28876a8f164381a20ae82e0918604285
Waddah Mortada <w.mortada87@gmail.com>
2020-08-10 19:43:44 +0100

    Fix Card textarea editing
    Also, use Bootstraps Form group

--- src/components/Action/Card/AddEdit.js
+++ src/components/Action/Card/AddEdit.js
@@ -1,14 +1,13 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import Confirm from '../../Confirm'
-import Button from 'react-bootstrap/Button'
+import { Form, Button } from 'react-bootstrap'
 import { faTrash, faCheck } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const AddEdit = props => {
   const isEdit = props.card && props.card.value
   const [inputText, setInputText] = useState(isEdit ? props.card.value : '')
-  const [rowsNumber, setRowsNumber] = useState(isEdit ? props.card.value.split('\n').length : 1)
   const [displayConfirm, setDisplayConfirm] = useState(false)
 
   const handleClose = () => setDisplayConfirm(false)
@@ -17,7 +16,7 @@ const AddEdit = props => {
   const addCard = () => {
     if (inputText) {
       setInputText('')
-      props.card.value = inputText
+      props.card.value = inputText.trim()
       props.cardFunctions.update(props.card)
     }
   }
@@ -28,27 +27,17 @@ const AddEdit = props => {
     handleClose()
   }
 
-  const updateInputSize = (event) => {
-    const { keyCode, target } = event
-    const rowsNum = target.value.split('\n').length
-
-    switch (keyCode) {
-      case 13: // Enter
-        setRowsNumber(rowsNumber + 1)
-        break
-      case 8: // Backspace
-        if (rowsNum !== rowsNumber) {
-          setRowsNumber((rowsNum > 0) ? rowsNum : 1)
-        }
-        break
-    }
+  const updateInputSize = (e) => {
+    e.target.style.height = 'inherit'
+    e.target.style.height = `${e.target.scrollHeight}px`
   }
 
-  // Fix AUTOFOCUS SELECTOR POINTER (should always start at the end)
   return (
     <form>
       {displayConfirm ? <Confirm type="delete" submit={deleteCard} show={displayConfirm} handleClose={handleClose} /> : null}
-      <textarea className="textareaInput columnInput" rows={rowsNumber} onKeyUp={updateInputSize} autoFocus value={inputText} onChange={e => setInputText(e.target.value)} />
+      <Form.Group>
+        <textarea className="textareaInput" onKeyUp={updateInputSize} onFocus={updateInputSize} autoFocus value={inputText} onChange={e => setInputText(e.target.value)} />
+      </Form.Group>
       <Button className="float-right submitCardButton" size="sm" variant="success" onClick={() => addCard()}>
         <FontAwesomeIcon className="icon-thumb" icon={faCheck} />
       </Button>
