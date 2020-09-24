1c07ee143dc377a28c089656b64319c9bf811b47
Waddah Mortada <w.mortada87@gmail.com>
2020-09-24 08:28:40 +0100

    Add copy actions to clip board feature

--- src/components/ActionsColumn.js
+++ src/components/ActionsColumn.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
-import { Card, Button } from 'react-bootstrap'
+import { Card, Button, Toast } from 'react-bootstrap'
 import { exportActionsToPdf, useFocus } from '../lib/helpers'
-import { faFileExport } from '@fortawesome/free-solid-svg-icons'
+import { faFileExport, faCopy } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const bullet = '\u2022'
@@ -12,6 +12,9 @@ const backspace = 8
 
 const ActionsColumn = props => {
   const [actionsRef, setActionsFocus] = useFocus()
+  const [showNotification, setShowNotification] = useState(false)
+
+  const toggleShowNotification = () => setShowNotification(!showNotification)
 
   useEffect(() => {
     if (!props.actions) props.setActions(bulletWithSpace)
@@ -49,12 +52,27 @@ const ActionsColumn = props => {
     props.socket.emit('setActions', newActions)
   }
 
+  const copyToClipboard = (e) => {
+    actionsRef.current.select()
+    document.execCommand('copy')
+    e.target.focus()
+    toggleShowNotification()
+  }
+
   return (
     <Card className="actionsBoardCard fullHeight">
+      <Toast className="actionsToasts" animation={true} show={showNotification} onClose={toggleShowNotification} delay={3000} autohide>
+        <Toast.Header>
+          <strong className="mr-auto">Copied to clipboard!</strong>
+        </Toast.Header>
+      </Toast>
       <Card.Header>
         <h5 className="inlineBlock">Actions</h5>
         <Button className="float-right" size="sm" variant="info" title="Export to PDF" onClick={() => exportActionsToPdf(props.actions)}>
           <FontAwesomeIcon className="icon-thumb" icon={faFileExport} />
+        </Button>
+        <Button className="float-right copy-button" size="sm" variant="copy" title="Copy to Clipboard" onClick={copyToClipboard}>
+          <FontAwesomeIcon className="icon-thumb" icon={faCopy} />
         </Button>
       </Card.Header>
       <Card.Body>
