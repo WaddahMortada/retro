882a7e3f62ee2bcfb144e22caf3082ffc8e566b8
Waddah Mortada <w.mortada87@gmail.com>
2021-03-22 10:30:16 +0000

    Add footer
    Tidy up the ui in general

--- src/components/Board.js
+++ src/components/Board.js
@@ -7,7 +7,7 @@ import AddColumn from './Action/Column/Add'
 import ActionsColumn from './ActionsColumn'
 import AdminSelector from './AdminSelector'
 import { Container, Row, Col, Button, Navbar, Nav, Card, Toast } from 'react-bootstrap'
-import { faChalkboard, faColumns, faListUl, faPlus, faMinus, faUsers } from '@fortawesome/free-solid-svg-icons'
+import { faChalkboard, faColumns, faListUl, faPlus, faMinus, faUsers, faGithub } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Board = props => {
@@ -139,9 +139,9 @@ const Board = props => {
           </Toast.Body>
         </Toast>
       </div>
-      <Row className="fullHeight">
+      <Row className="fullHeight contentBoard">
         <Col md={{ span: showActions ? 9 : 12 }}>
-          <Row className="fullHeight">
+          <Row className="fullHeight" style={{ height: '85vh', paddingBottom: '2px' }}>
             <Col>
               {showConfirm ? <Confirm type="reset" submit={resetBoard} show={showConfirm} handleClose={handleCloseConfirm} /> : null}
               <Card className="boardCard fullHeight">
@@ -162,6 +162,17 @@ const Board = props => {
         </Col>
         <Col className={' ' + (showActions ? 'show' : 'hide')} md={{ span: 3 }}>
           {showActions ? <ActionsColumn showActions={showActions} socket={props.socket} actions={props.actions} setActions={props.setActions} board={props.board} /> : null}
+        </Col>
+      </Row>
+      <Row>
+        <Col style={{ width: '100%', position: 'fixed', left: 0, bottom: 0, padding: 0 }}>
+          <Navbar className="navBar" bg="dark" variant="dark">
+            <Nav>
+              <a href="https://github.com/WaddahMortada">
+                <FontAwesomeIcon className="icon-thumb" icon={faGithub} />
+              </a>
+            </Nav>
+          </Navbar>
         </Col>
       </Row>
     </div>
--- src/components/TempleteSelector.js
+++ src/components/TempleteSelector.js
@@ -74,7 +74,7 @@ const TemplateSelector = props => {
           <Logo templateSelector={true} />
           {/* <AdminSelector admin={props.admin} setAdmin={props.setAdmin} /> */}
         </Navbar>
-        <Card className="templateCard">
+        <Card className="templateCard" style={{ height: '85vh' }}>
           <Card.Body>
             <form onSubmit={create}>
               <Row className="justify-content-md-center">
